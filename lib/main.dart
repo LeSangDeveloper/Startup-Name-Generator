57d4da8feeb9a74946c1ad94cf178dc5e4d690aa
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2022-11-22 09:49:51 +0800

    init class Word Generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,16 +10,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext ctx) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: "Startup Name Generator",
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Startup Name")
         ),
-        body: const Center(
-          child: Text("Apple")
+        body: Center(
+          child: Text(wordPair.asPascalCase)
         )
       ),
     );
+  }
+}
+
+class WordGenerator extends StatefulWidget {
+  const WordGenerator({Key? key}) : super(key: key);
+
+  @override
+  State<WordGenerator> createState() => _WordGeneratorState();
+}
+
+class _WordGeneratorState extends State<WordGenerator> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (BuildContext context, int idx) {
+        return const Divider();
+    });
   }
 }
